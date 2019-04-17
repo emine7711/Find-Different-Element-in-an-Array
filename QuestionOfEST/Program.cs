25fe50e75b527770a73790c526cbcdbbaaee7c45
emine7711 <36545759+emine7711@users.noreply.github.com>
2019-04-17 22:35:00 +0300

    Find Different Element in an Array
    finds only one element which is different in an array with just one loop and just one if condition.

---
+++ QuestionOfEST/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionOfEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] est = { "x", "x", "x", "x", "x", "x", "x","y" };
+
+            int a = 0;
+            for (int i = 1; i <= est.Length; i++)
+            {
+                if (est[a]!=est[est.Length - i] && est[i]!= est[est.Length-i])
+                {
+                    a = est.Length - i;
+                    break;
+                }
+                
+            }
+            Console.WriteLine("Farklı eleman " + (a) + ". indistedir.");
+            Console.ReadLine();
+        }
+    }
+}
